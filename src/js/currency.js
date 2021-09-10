c1d46778b8159c1c99db4da8851851ec44c9f966
Art Weinstein <artur.weinstein@gmail.com>
2021-09-10 11:19:37 -0700

    WIP properly import convert.js to main.js

--- src/js/currency.js
+++
@@ -1,9 +0,0 @@
-export default class chooseCurrency {
-  static chosenCurrency (){
-      $(document).ready(function() {
-      $('#choose').click(function(currency) {
-        let currency = $('#currency').val();
-      });
-    });
-  }
-}
--- src/main.js
+++ src/main.js
@@ -2,28 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import {chooseCurrency} from './js/currency.js';
+// import conversion from './currency.js'
 
+function convert (response, currency) {
+  $('#convert').html(`USD in ${currency} comes out to ${response.conversion_rates[currency]}`);
+}
 
 $(document).ready(function() {
-  $('#convert').click(function() {
-    // const currency = $('#currency').val();
-
-    let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-
-    request.open("GET", url, true);
-    request.send();
-
-  function getElements(response) {
-      $('.pln').html(`${response.conversion_rates.$(chooseCurrency)}`);
-    }
+  $('#convert').click(function(event) {
+    event.preventDefault;
+    let dollar = $('#amount').val();
+    $('#amount').val();
+    let currency = $("#currency").val();
+    conversion.convert()
+      .then(function(response){
+        convert(response, currency);
+      });
   });
 });

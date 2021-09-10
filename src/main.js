35522520cf93fdc7ccaef9e3549729a41d074459
Art Weinstein <artur.weinstein@gmail.com>
2021-09-10 09:06:36 -0700

    create initial in the main.js to convert USD to PLN.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
+
+
+$(document).ready(function() {
+  $('#convert').click(function() {
+    const currency = $('#currency').val();
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+  function getElements(response) {
+      $('.pln').html(``);
+    }
+  });
+});

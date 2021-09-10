04403c31078eb11a1391ca8508252def8d641e5c
Art Weinstein <artur.weinstein@gmail.com>
2021-09-10 11:46:17 -0700

    WIP: work on properly reading conversion function.

--- src/currency.js
+++ src/currency.js
@@ -1,4 +1,4 @@
-export default class conversion {
+export class Conversion {
   static convertCurrency() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
--- src/main.js
+++ src/main.js
@@ -2,21 +2,38 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import conversion from './currency.js'
+import Conversion from './currency.js';
 
-function convert (response, currency) {
-  $('#convert').html(`USD in ${currency} comes out to ${response.conversion_rates[currency]}`);
+function convert (response, dollar, currency) {
+  $('#convert').html(`${dollar} in ${currency} comes out to ${response.conversion_rates[currency]}`);
 }
+
+// $(document).ready(function() {
+//   $('#convert').click(function(event) {
+//     event.preventDefault;
+//     // let dollar = 0;
+//     // dollar = $('#amount').val();
+//     $('#amount').val();
+//     let currency = $("#currency").val();
+//     Conversion.convert()
+//       .then(function(response){
+//         convert(response, currency);
+//       });
+//   });
+// });
 
 $(document).ready(function() {
   $('#convert').click(function(event) {
-    event.preventDefault;
+    event.preventDefault();
+
     let dollar = $('#amount').val();
-    $('#amount').val();
-    let currency = $("#currency").val();
-    conversion.convert()
-      .then(function(response){
-        convert(response, currency);
+    // console.log(usDollar);
+    $('#dollar').val("");
+    let currency = $('#currency').val();
+    console.log(currency);
+    Conversion.convert()
+      .then(function(response) {
+        convert(response, dollar, currency);
       });
   });
 });
